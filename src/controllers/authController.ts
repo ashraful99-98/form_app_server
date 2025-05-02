f96abf31159a142c403c4479f1b1a059b40c40f8
ashraful99-98 <ashrafulkamran98@gmail.com>
2025-05-02 16:29:16 +0600

    added all

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -125,10 +125,11 @@ export const login = async (
       message: "Login successful",
       token,
       user: {
-        id: user._id,
+        _id: user._id,
         name: user.name,
         email: user.email,
         role: user.role,
+        token
       },
     });
   } catch (err) {
